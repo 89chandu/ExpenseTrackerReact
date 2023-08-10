16dcd1c2a60f19a03ef54aa3e2440530ac0f9454
Chandu Bopche <Chandubopche321@gmail.com>
2023-08-10 20:47:38 +0530

    finalcommit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,30 +1,21 @@
-import React, {useState} from 'react';
-import ExpenseDate from '../Expenses/ExpenseDate';
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-import '../Expenses/ExpenseItem.css';
+import './ExpenseItem.css';
 
-function ExpenseItem(props) {
-
-  const [title,setTitle] = useState(props.title); // it must be called inside function
-
-  const clickHandler = () => {
-   setTitle("updated");
-    console.log("ExpenseItem evaluted by React");
-    
-  };
-
+const ExpenseItem = (props) => {
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} /> {/* Pass the date prop to ExpenseDate component */}
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-      </div>
-      <div className='expense-item__price'>{props.amount}
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
-    
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,32 +1,25 @@
 import React from 'react';
 
 import ExpenseItem from './ExpenseItem';
-import "../Expenses/ExpensesList.css";
+import './ExpensesList.css';
 
 const ExpensesList = (props) => {
-    let expensesContent = <p> No Expenses found here.</p>;
+  if (props.items.length === 0) {
+    return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
+  }
 
-
-    if(props.items.length === 0) {
-        return <h2 className='expenses-list__fallback'>Found no expenses</h2>
-    }
-
-    return <ul className='expenses-list'>
-
-        { props.items.map((expense) => (
-
-<ExpenseItem>
-    key = {expense.id}
-    title = {expense.title}
-    amount = {expense.amount}
-    date = {expense.date}
-</ExpenseItem>
-))};
-
-        
-
+  return (
+    <ul className='expenses-list'>
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </ul>
-
+  );
 };
 
 export default ExpensesList;
